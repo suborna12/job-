488a8816b802532135df3460b54e5d9dd52a7a0b
suborna12 <153237224+suborna12@users.noreply.github.com>
2025-06-20 10:56:52 -0700

    Add files via upload

---
+++ user.controller.js
@@ -0,0 +1,178 @@
+import  User  from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(404).json({
+        message: "Missing required fields",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "Email already exists",
+        success: false,
+      });
+    }
+    //convert password to hashes
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      fullname,
+      email,
+      phoneNumber,
+      adharcard,
+      pancard,
+      password: hashedPassword,
+      role,
+    });
+    return res.status(200).json({
+      message: `Account created successfully ${fullname}`,
+      success: true,
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server Error registering user",
+      success: false,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res.status(404).json({
+        message: "Missing required fields",
+        success: false,
+      });
+    }
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    if (user.role !== role) {
+      return res.status(403).json({
+        message: "You don't have the necessary role to access this resource",
+        success: false,
+      });
+    }
+
+    //token generate
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    User = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile:user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "Strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullname}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server Error login failed",
+      success: false,
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server Error logging out",
+      success: false,
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, bio, skills } = req.body;
+    const file = req.files;
+    if (!fullname || !email || !phoneNumber || !bio || !skills) {
+      return res.status(404).json({
+        message: "Missing required feilds",
+        success: false,
+      });
+    }
+    const skillsArray = skills.split(',');
+    const userId = req.id;
+    let user = await User.findById (userId);
+     if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+    user.fullname = fullname;
+    user.email = email;
+    user.phoneNumber = phoneNumber;
+    user.bio = bio;
+    user.skills = skillsArray;
+
+    await user.save()
+     user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+    return res.status(200).json({
+        message: "Profile updated successfully",
+        user,
+        success: true,
+      });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server Error updating profile",
+      success: false,
+    });
+  }
+};
